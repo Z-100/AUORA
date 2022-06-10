8ff873cb051a90c6a6ab932b457e904286beb5e5
Marvin <marvin131t@gmail.com>
2022-06-10 10:57:09 +0200

    Basic layout upgrade

---
+++ src/main/java/com/auora/api/service/impl/PasswordValidationService.java
@@ -0,0 +1,12 @@
+package com.auora.api.service.impl;
+
+import com.auora.api.components.account.entity.Account;
+import com.auora.api.service.IPasswordValidationService;
+
+public class PasswordValidationService implements IPasswordValidationService {
+
+	@Override
+	public Account validate(String email, String password) {
+		return null;
+	}
+}
