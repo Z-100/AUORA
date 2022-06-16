d75e142af21cbcce0af3b3058669e6d0e9c45da8
Marvin <marvin131t@gmail.com>
2022-06-16 08:51:00 +0200

    Fix typo

--- src/main/java/com/auora/AuoraApplication.java
+++ src/main/java/com/auora/AuoraApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuoraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AuoraApplication.class, args);
 	}
 
 }
