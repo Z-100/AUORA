b97e65ca8748043dcd34bff09043fc68312fbdfc
Marvin <marvin131t@gmail.com>
2022-06-17 08:13:40 +0200

    Added mapper unit tests

---
+++ src/test/java/com/auora/api/components/account/services/mapper/AAccountMapperTest.java
@@ -0,0 +1,20 @@
+package com.auora.api.components.account.services.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AAccountMapperTest {
+
+	@Test
+	void toDTO() {
+		// Should not need unit test, as project wouldn't compile on error
+		assertTrue(true);
+	}
+
+	@Test
+	void toEntity() {
+		// Should not need unit test, as project wouldn't compile on error
+		assertTrue(true);
+	}
+}
